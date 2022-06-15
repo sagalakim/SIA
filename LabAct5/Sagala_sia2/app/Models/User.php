fe2cc9dbc6dc92188ed698288e67890f7c666550
sagalakim <kimjohn.sagala02@gmail.com>
2022-06-15 10:34:03 +0800

    Act5

---
+++ LabAct5/Sagala_sia2/app/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+ namespace App\Models;
+ use Illuminate\Database\Eloquent\Model;
+
+ class User extends Model{
+ protected $table = 'tbluser';
+ // column sa table
+ protected $fillable = [
+ 'id','username', 'password', 'gender', 'jobid'
+ ];
+ public $timestamps = false;
+ protected $primarykey = 'id';
+ protected $hidden = [
+    'password',
+];
+ }
---
+++ LabAct5/Sagala_sia2/app/Models/UserJob.php
@@ -0,0 +1,12 @@
+<?php
+
+	namespace App\Models;
+	use Illuminate\Database\Eloquent\Model;
+
+	class UserJob extends Model {
+		protected $table = 'tbluserjob';
+		protected $fillable = ['jobid', 'jobname', ];
+	
+	public $timestamps = false;
+	protected $primaryKey = 'jobid';
+	}
