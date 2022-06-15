5db42c49310d36bd2536f3ec3059e1a2f08b54ed
sagalakim <kimjohn.sagala02@gmail.com>
2022-06-15 14:15:43 +0800

    L

--- LabAct5/Sagala_sia1/bootstrap/app.php
+++ LabAct5/Sagala_sia1/bootstrap/app.php
@@ -27,6 +27,8 @@ $app = new Laravel\Lumen\Application(
 
  $app->withEloquent();
 
+ $app->configure('database');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
